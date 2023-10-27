69d4832a8d60e0ee49826b674c004cb293c825dd
Munawir76 <munawirsajali76@gmail.com>
2023-10-27 18:15:55 +0700

    fix: header

--- app/components/layout/header.js
+++ app/components/layout/header.js
@@ -3,27 +3,27 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="fixed h-full w-full">
-      <div className="bg-yellow-400 flex justify-between ">
+    <div className="fixed h-full w-full z-10">
+      <div className=" flex justify-between ">
         <div className="text-black text-2xl font-bold leading-7 cursor-pointer p-5">
          <Link href="/">
          <span>My Portfolio</span>
          </Link>
         </div>
-        <div className="flex justify-evenly mr-10">
-          <div className="p-5 hover:bg-white cursor-pointer">
-            <Link href="/about">
-              <span className=" text-black">About</span>
+        <div className="flex justify-evenly mr-40">
+          <div className="p-5  cursor-pointer">
+            <Link  href="/about" >
+              <span className="hover:text-white text-black">About</span>
             </Link>
           </div>
-          <div className="p-5 hover:bg-white cursor-pointer">
+          <div className="p-5  cursor-pointer">
             <Link href="/project">
-              <span className=" text-black">Project</span>
+              <span className="hover:text-white text-black">Project</span>
             </Link>
           </div>
-          <div className="p-5 hover:bg-white cursor-pointer">
+          <div className="p-5 cursor-pointer">
             <Link href="/contact">
-              <span className=" text-black">Contact</span>
+              <span className="hover:text-white text-black">Contact</span>
             </Link>
           </div>
         </div>
--- app/page.js
+++ app/page.js
@@ -1,14 +1,19 @@
 import React from "react";
 import LayoutMain from "./components/layout/layoutMain";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <LayoutMain>
      <div className="h-screen w-full bg-white">
-      <div className=" e">
-      <span>Ini Konten</span>
+      <div className="absolute top-0 right-0">
+      <Image src="/asset/yellow-bg.png" width={777} height={877}/>
+      </div>
+      <div className="flex ">
+        <span className="w-[486px] h-[408px] text-black bg-red-500">ajbdkiab</span>
       </div>
      </div>
+     
     </LayoutMain>
   );
 }
