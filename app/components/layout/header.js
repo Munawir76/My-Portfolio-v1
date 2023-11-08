645d5b78ac02e45d768015add0d796c2d5e74111
munawirsajali76 <munawirsajali76@gmail.com>
2023-11-09 01:58:08 +0700

    fix: Layout

--- app/components/layout/header.js
+++ app/components/layout/header.js
@@ -4,15 +4,15 @@ import React from "react";
 export default function Header() {
   return (
     <div className="fixed h-full w-full z-10">
-      <div className=" flex justify-between ">
-        <div className="text-black text-2xl font-bold leading-7 cursor-pointer p-5">
-         <Link href="/">
-         <span>My Portfolio</span>
-         </Link>
+      <div className=" flex justify-between bg-white">
+        <div className="text-black text-2xl font-bold cursor-pointer p-5 ">
+          <Link href="/">
+            <span className="ml-20">My Portfolio</span>
+          </Link>
         </div>
         <div className="flex justify-evenly mr-40">
           <div className="p-5  cursor-pointer">
-            <Link  href="/about" >
+            <Link href="/about">
               <span className="hover:text-white text-black">About</span>
             </Link>
           </div>
--- app/page.js
+++ app/page.js
@@ -5,15 +5,34 @@ import Image from "next/image";
 export default function Home() {
   return (
     <LayoutMain>
-     <div className="h-screen w-full bg-white">
-      <div className="absolute top-0 right-0">
-      <Image src="/asset/yellow-bg.png" width={777} height={877}/>
+      <div className="h-screen w-full bg-white">
+        <div className="absolute top-0 right-0">
+          <Image src="/asset/yellow-bg.png" width={777} height={877} />
+        </div>
+        <div className="absolute top-[110px] left-[104px] ">
+          <div className="w-[400px] h-[408px] text-black flex flex-col content-between">
+            <span className="text-[#FDC435] font-bold text-[20px]">
+              Frontend Web Developer
+            </span>
+            <span className="text-black font-bold text-[44px]">
+              Hello, my name is Madelyn Torff
+            </span>
+            <span className="text-black font-light text-[24px] mt-6">
+              Short text with details about you, what you do or your
+              professional career. You can add more information on the about
+              page.
+            </span>
+            <div className="flex justify-start gap-5 mt-6">
+              <button className="cursor-pointer p-2 w-max bg-[#FDC435] rounded-md font-medium">
+                Projects
+              </button>
+              <button className="cursor-pointer p-2 w-max outline outline-2 rounded-md font-medium">
+                Linkedin
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="flex ">
-        <span className="w-[486px] h-[408px] text-black bg-red-500">ajbdkiab</span>
-      </div>
-     </div>
-     
     </LayoutMain>
   );
 }
